d10b5676e0dee951fa0760b42286e1b90688a602
Dillon Tarr <dillon.tarr@kroger.com>
2024-09-13 09:32:10 -0400

    First dependencies added.

---
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,28 @@
+package com.pluralsight.courseinfo.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CourseRetriever {
+    public static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String... args) {
+        LOG.info("CourseRetriever is starting...");
+        if (args.length == 0) {
+            LOG.warn("Please provide an author name as first argument.");
+            return;
+        }
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            LOG.error("Unexpected error", e);
+        }
+
+    }
+
+    private static void retrieveCourses(String authorId) {
+        LOG.info("Retrieving courses for author '{}'.", authorId);
+    }
+
+
+}
